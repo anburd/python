04f2f79f2825a43ec560f1d3a83c1d4eebfb619d
anburd <anburd@gmail.com>
2022-10-20 15:35:36 +0400

    20-10-22

--- task33.py
+++ task33.py
@@ -2,30 +2,37 @@
 # (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
 # Пример:
 # k=2 => 2x² + 4x + 5 = 0 или x² + 5 = 0 или 10x² = 0
-from random import randint as rnd
+from random import randint 
+
 k=2
 
-lst_k = [rnd(0, 1) for i in range(k+1)]
 
-    
 
-lst= lst_k[::-1]
-wr = ''
-if len(lst) < 1:
-    wr = 'x = 0'
-else:
-    for i in range(len(lst)):
-        if i != len(lst) - 1 and lst[i] != 0 and i != len(lst) - 2:
-            wr += f'{lst[i]}x^{len(lst)-i-1}'
-            if lst[i+1] != 0 or lst[i+2] != 0:
-                wr += ' + '
-        elif i == len(lst) - 2 and lst[i] != 0:
-            wr += f'{lst[i]}x'
-            if lst[i+1] != 0 or lst[i+2] != 0:
-                wr += ' + '
-        elif i == len(lst) - 1 and lst[i] != 0:
-            wr += f'{lst[i]} = 0'
-        elif i == len(lst) - 1 and lst[i] == 0:
-            wr += ' = 0'
+lst_k = [randint(0, 100) for i in range(k+1)]
 
-print(wr)
+print(lst_k)
+   
+polynomial = str(lst_k[0])+'x^2 + ' + str(lst_k[1]) + 'x + ' + str(lst_k[2]) + ' = 0'
+
+print(polynomial)
+
+lst_step=[]
+
+for i in range(0, k+1):
+    lst_step.append('x^' + str(k-i))
+
+print(lst_step)
+
+poly=[]
+for i in range(0, k+1):
+    print(lst_k[i])
+    print(lst_step[i])
+
+    poly.append(str(lst_k[i]) + str(lst_step[i]))
+
+poly_str=' + '.join(poly)
+
+print(poly_str)
+poly_str=poly_str.replace('x^1', 'x').replace('x^0', '')+' = 0'
+
+print(poly_str)
