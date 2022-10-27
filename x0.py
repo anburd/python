8bbb59617b6187ea5fb3354bd3c4ec9d7d08d50c
anburd <anburd@gmail.com>
2022-10-27 10:43:11 +0400

    27-10-99

--- x0.py
+++ x0.py
@@ -38,27 +38,28 @@ else:
 
 def printGameArea():
     """
-    Функция выводит на экран игровое поле1
+    Функция выводит на экран игровое поле
 
     """
-    not_areas = ['a1', 'a2', 'a3', 'b1', 'b2', 'b3', 'c1', 'c2', 'c3']
-    cor=('a','','','b','','', 'c','','')    
+  
     os.system('cls' if os.name == 'nt' else 'clear')
+    cor=('a','b','c') 
+    not_areas = ['a1', 'a2', 'a3', 'b1', 'b2', 'b3', 'c1', 'c2', 'c3']
     print('   ', '1', ' ', '2', ' ', '3', '   ')
-    for i in range(0, 9, 3):
-
-        print(' ','-' * 13)
-        if areas[i] not in not_areas:
-            a1=areas[i]
-        else: a1=' '
-        if areas[i+1] not in not_areas:
-            a2=areas[i+1]
-        else: a2=' '
-        if areas[i+2] not in not_areas:
-            a3=areas[i+2]
-        else: a3=' '
-        print(cor[i], '|', a1, '|', a2, '|', a3, '|')
     print(' ','-' * 13)
+    for i in range(3):
+        if areas[0+i*3] not in not_areas:
+            a1=areas[0+i*3]
+        else: a1=' '
+        if areas[1+i*3] not in not_areas:
+            a2=areas[1+i*3]
+        else: a2=' '
+        if areas[2+i*3] not in not_areas:
+            a3=areas[2+i*3]
+        else: a3=' '
+        print(cor[i], "|", a1, "|", a2, "|", a3, "|")
+        print(' ','-' * 13)
+
 
 
 def move():
