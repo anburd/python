825f1c0bd1f4a886305901eb398d7bf9451fea76
anburd <anburd@gmail.com>
2022-11-16 14:26:36 +0400

    16-11-22

--- phone/import_data.py
+++ phone/import_data.py
@@ -1,7 +1,4 @@
-import datetime
-import record
-
 def input_format(entry):
 
     with open('phones.csv', 'a', encoding = 'utf-8') as file:
-        file.write(f'{entry[0]};{entry[1]};{entry[2]};{entry[3]}\n\n')
+        file.write(f'{entry[0]};{entry[1]};{entry[2]};{entry[3]};{entry[4]}\n\n')
--- phone/record.py
+++ phone/record.py
@@ -1,12 +1,1 @@
-def record():
-    entry = []
-    surname = input('Введите фамилию: ')
-    entry.append(surname)
-    name = input('Введите имя: ')
-    entry.append(name)
-    phone = input('Введите телефон: ')
-    entry.append(phone)
-    description = input('Введите описание контакта: ')
-    entry.append(description)
-    print('Вами введена запись: ', entry)
-    return entry
+
